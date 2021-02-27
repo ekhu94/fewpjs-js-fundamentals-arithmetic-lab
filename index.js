1604e3d1923c0d24be021d19f56dd0c960acd457
Erik Huang <erikhuangdrums@gmail.com>
2021-02-26 17:50:41 -0800

    commit tree dcdc2ab6da2bbd44fb186132807d13213ed42667

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 30, num2 = 32;
+const multiply = num1 + num2
 
-// Write your code here
+const random = Math.floor(Math.random() + 1)
+
+const num3 = 14, num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(20, 10)
